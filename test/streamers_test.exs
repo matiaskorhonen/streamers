06128a33799daf1137eceeb2ad65c5381f3468a2
Matias Korhonen <matias@kiskolabs.com>
2013-07-10 22:18:51 +0300

    Another thing!

--- test/streamers_test.exs
+++ test/streamers_test.exs
@@ -18,5 +18,6 @@ defmodule StreamersTest do
     m3u8s = Streamers.extract_m3u8(@index_file)
     assert Enum.first(m3u8s) ==
       M3U8[program_id: 1, bandwidth: 110000, path: "test/fixtures/emberjs/8bda35243c7c0a7fc69ebe1383c6464c.m3u8"]
+    assert length(m3u8s) == 5
   end
 end
